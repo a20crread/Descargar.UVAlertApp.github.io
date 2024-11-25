09ddb65eca3cb7a6869c97753146ac988691af32
a20crread <a20crread@iesgrancapitan.org>
2024-11-25 19:24:28 +0100

    Update particle.js

--- particle.js
+++ particle.js
@@ -1,29 +1,30 @@
-class particula{
-
-    constructor(x,y){
-        this.x = x
-        this.y = y
-        this.radio = 10
-        this.dirX = (Math.random() * 2) - 1
-        this.dirY = (Math.random() * 2) - 1
-        this.velocidad = 5
-    }
-    dibujar(){
-        ctx.beginPath()
-        ctx.arc(this.x,this.y,this.radio,0,Math.PI * 2)
-        ctx.fill()
-        ctx.closePath()
+export class Particula {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.radio = 10;
+        this.dirX = (Math.random() * 2) - 1;
+        this.dirY = (Math.random() * 2) - 1;
+        this.velocidad = 5;
     }
 
-    mover(){
-        this.x += this.dirX * this.velocidad
-        this.y += this.dirY * this.velocidad
+    dibujar(ctx) {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radio, 0, Math.PI * 2);
+        ctx.fillStyle = "rgba(255, 255, 255, 0.6)";
+        ctx.fill();
+        ctx.closePath();
+    }
 
-        if(this.x + this.radio > canvas.width || thix.x < 0){
-            this.dirX *= -1
+    mover(canvas) {
+        this.x += this.dirX * this.velocidad;
+        this.y += this.dirY * this.velocidad;
+
+        if (this.x + this.radio > canvas.width || this.x - this.radio < 0) {
+            this.dirX *= -1;
         }
-        if(this.y + this.radio > canvas.height || thix.y < 0){
-            this.dirY *= -1
+        if (this.y + this.radio > canvas.height || this.y - this.radio < 0) {
+            this.dirY *= -1;
         }
     }
 }
